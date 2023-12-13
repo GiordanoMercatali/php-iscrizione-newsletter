73a8904b0a9a16a15fd6ccb21619d69c3e46b71c
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-13 16:37:29 +0100

    add: display email (checks if it has '.' and '@' symbols)

--- index.php
+++ index.php
@@ -6,6 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="index.php" method="POST">
+        <input type="email" name="email-address" class="form-control" placeholder="your@email.com">
+        <button type="submit">Submit</button>
+    </form>
+
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $address = $_POST['email-address'];
+        if (empty($address)) {
+            echo "Insert e-mail address";
+        } else if (!str_contains($address, '@') || !str_contains($address, '.')) {
+            echo "Insert a valid address";
+        } else {
+            echo $address;
+        }
+        }
+    ?>
+
 </body>
 </html>
