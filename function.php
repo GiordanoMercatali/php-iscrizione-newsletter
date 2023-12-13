bc3dfaaeda95505601944bf23f026673a7bbc444
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-13 19:54:08 +0100

    add: class changes depending on result

--- function.php
+++ function.php
@@ -7,15 +7,15 @@
 
 <div>
     <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $address = $_POST['email-address'];
-            if (empty($address)) {
-                echo "Insert e-mail address";
-            } else if (!str_contains($address, '@') || !str_contains($address, '.')) {
-                echo "Insert a valid address";
-            } else {
-                echo "Welcome " . $address;
-            }
-        }
-    ?>
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {?>
+            <?php $address = $_POST['email-address']; ?>
+            <?php if (empty($address)) { ?>
+                <p class="alert alert-danger">Insert e-mail address</p>
+            <?php } else if (!str_contains($address, '@') || !str_contains($address, '.')) { ?>
+                <p class="alert alert-warning">Insert a valid address</p>
+            <?php } else { ?>
+                <p class="alert alert-primary">Welcome <?php echo $address ?> </p>
+                <?php } ?>
+            <?php } ?>
+    
 </div>
